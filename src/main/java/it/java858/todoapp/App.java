03f234429c78de04367cbf3c4072442d5e62921e
tss <tss@debian-tss24>
2016-06-09 21:41:18 +0200

    prima bozza

---
+++ src/main/java/it/java858/todoapp/App.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.java858.todoapp;
+
+import it.java858.todoapp.gui.Main;
+
+/**
+ *
+ * @author tss
+ */
+public class App {
+
+    public static void main(String[] args) {
+             
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Main().setVisible(true);
+            }
+        });
+                
+    }
+}
